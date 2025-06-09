7208c71bea375af53603c202c1345876c4ef8798
Sales Pitch Analyzer <project@example.com>
2025-06-09 10:27:02 -0600

    Limpieza: mover y mejorar .gitignore, limpiar archivos ignorados

--- src/app/transcriber_base.py
+++
@@ -1,3 +0,0 @@
-class BaseTranscriber:
-    def transcribe(self, audio_path):
-        raise NotImplementedError("Debes implementar el método transcribe en la subclase.") 
--- src/config/gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-# gunicorn.conf.py
-# Configuración para aumentar el timeout a 300 segundos
- 
-bind = "0.0.0.0:10000"
-workers = 4
-timeout = 120
-pythonpath = "src"
-wsgi_app = "app.app:app" 
